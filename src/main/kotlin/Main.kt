ce1be708d9a4255fbd9db9c596e834748d26ef6f
44x1carbon <b4079@oic.jp>
2018-03-25 02:16:20 +0900

    FreeMarker を設定する close #1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
+import freemarker.cache.ClassTemplateLoader
 import io.ktor.application.*
-import io.ktor.http.*
+import io.ktor.freemarker.FreeMarker
+import io.ktor.freemarker.FreeMarkerContent
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
@@ -7,14 +9,20 @@ import io.ktor.server.netty.*
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, port = 8080) {
+        install(FreeMarker) {
+            templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        }
+
         routing {
             get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
+                call.respond(FreeMarkerContent("welcome.ftl", mapOf<String, Any>()))
             }
             get("/demo") {
                 call.respondText("HELLO WORLD!")
             }
         }
+
+        return@embeddedServer
     }
     server.start(wait = true)
 }
