02a0f3a6e558527b2f4960d8803161f667bf2327
44x1carbon <b4079@oic.jp>
2018-03-25 02:35:36 +0900

    Locationsを設定する close #5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,11 @@ import freemarker.cache.ClassTemplateLoader
 import io.ktor.application.*
 import io.ktor.freemarker.FreeMarker
 import io.ktor.freemarker.FreeMarkerContent
+import io.ktor.locations.Location
+import io.ktor.locations.Locations
+import io.ktor.locations.get
 import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.routing.routing
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
@@ -12,13 +15,13 @@ fun main(args: Array<String>) {
         install(FreeMarker) {
             templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
         }
+        install(Locations)
+
+        @Location("/") data class Welcome(val p: Int? = null)
 
         routing {
-            get("/") {
+            get<Welcome> {
                 call.respond(FreeMarkerContent("welcome.ftl", mapOf<String, Any>()))
-            }
-            get("/demo") {
-                call.respondText("HELLO WORLD!")
             }
         }
 
