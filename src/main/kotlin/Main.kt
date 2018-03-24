74495bc26acbc5df178ffc78a5f73f078aada0c4
44x1carbon <b4079@oic.jp>
2018-03-25 04:36:36 +0900

    Exposedを設定する close #2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,8 +9,18 @@ import io.ktor.response.*
 import io.ktor.routing.routing
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.sql.Connection
 
 fun main(args: Array<String>) {
+
+    Database.connect("jdbc:sqlite:/Users/user/development/ktor-template/src/main/resources/database.sqlite", "org.sqlite.JDBC")
+    transaction(transactionIsolation = Connection.TRANSACTION_SERIALIZABLE, repetitionAttempts = 3) {
+        logger.addLogger(StdOutSqlLogger)
+    }
+
     val server = embeddedServer(Netty, port = 8080) {
         install(FreeMarker) {
             templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
