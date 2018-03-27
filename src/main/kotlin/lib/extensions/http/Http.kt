fc3ec90e530f135fc41a4d149f0e3ad8092e9181
44x1carbon <b4079@oic.jp>
2018-03-27 12:30:19 +0900

    Parameterインスタンスを任意の方に変換するメソッド追加

---
+++ src/main/kotlin/lib/extensions/http/Http.kt
@@ -0,0 +1,26 @@
+package lib.extensions.http
+
+import io.ktor.http.Parameters
+import io.ktor.util.DefaultConversionService
+import java.lang.reflect.Type
+import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.javaType
+
+inline fun <reified T: Any?> Parameters.to(): T {
+    val klass: KClass<*> = T::class
+    val constructor = klass.primaryConstructor ?: klass.constructors.single()
+    val arguments = constructor.parameters.map { parameter ->
+        val type = parameter.type
+        val name = parameter.name!!
+        val value = conversionValue(name, type.javaType)
+        parameter to value
+    }.filterNot { it.first.isOptional && it.second == null }.toMap()
+
+    return constructor.callBy(arguments) as T
+}
+
+fun Parameters.conversionValue(name: String, type: Type): Any?{
+    val values = getAll(name)
+    return values?.let { DefaultConversionService.fromValues(values, type) }
+}
