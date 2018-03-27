1ed7b2f471d9534afc0066d118dd307981fb3cc7
44x1carbon <b4079@oic.jp>
2018-03-27 17:47:06 +0900

    対応していない型の場合のExceptionを定義

--- src/main/kotlin/lib/validation/rules/Rule.kt
+++ src/main/kotlin/lib/validation/rules/Rule.kt
@@ -4,3 +4,5 @@ interface Rule<Target: Any?> {
     val name: String get() = this::class.simpleName ?: throw Throwable("")
     fun valid(value: Target): Boolean
 }
+
+object NotSupportClass: Throwable("not support class")
