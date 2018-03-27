b84de1df991ad2f11191edb71b51385d638caeec
44x1carbon <b4079@oic.jp>
2018-03-27 15:52:43 +0900

    プロパティのバリデーションをするためのインタフェースを定義

---
+++ src/main/kotlin/lib/validation/Verifiable.kt
@@ -0,0 +1,22 @@
+package lib.validation
+
+import lib.validation.rules.Rule
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.memberProperties
+
+interface Verifiable {
+    val rules: Map<String, List<Rule<*>>>
+
+    fun valid() = validateProperties.map {
+        val rules: List<Rule<Any?>> = rules.get(it.name)?.map { it as Rule<Any?> } ?: listOf()
+        val value: Any? = it.get(this)
+        it.name to rules.filter { it.valid(value).not() }.map { it.name }
+    }.toMap()
+
+    private val validateProperties: List<KProperty1<Verifiable, Any?>>
+        get() = this::class.memberProperties.filter { Verifiable::class.memberProperties.map { it.name }.contains(it.name).not() }.map { it as KProperty1<Verifiable, Any?> }
+
+    private val KProperty1<out Verifiable, Any?>.validateName: String
+        get() = findAnnotation<ValidationProperty>()!!.name
+}
---
+++ src/main/kotlin/lib/validation/rules/Rule.kt
@@ -0,0 +1,6 @@
+package lib.validation.rules
+
+interface Rule<Target: Any?> {
+    val name: String get() = this::class.simpleName ?: throw Throwable("")
+    fun valid(value: Target): Boolean
+}
