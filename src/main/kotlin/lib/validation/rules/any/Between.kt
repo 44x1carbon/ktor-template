ff94396b91feda17a7dec8dd8184ac1d1dd532db
44x1carbon <b4079@oic.jp>
2018-03-27 18:31:57 +0900

    開始値、終了値を引数にとるセカンダリーコンストラクタを定義

--- src/main/kotlin/lib/validation/rules/any/Between.kt
+++ src/main/kotlin/lib/validation/rules/any/Between.kt
@@ -4,6 +4,8 @@ import lib.validation.rules.Rule
 import lib.validation.rules.size
 
 class Between<T: Any?>(val range: IntRange): Rule<T> {
+    constructor(start: Int, end: Int) : this((start..end))
+
     override fun valid(value: T): Boolean = when(value) {
         null -> false
         else -> range.contains(value.size)
--- src/main/kotlin/lib/validation/rules/integer/DigitsBetween.kt
+++ src/main/kotlin/lib/validation/rules/integer/DigitsBetween.kt
@@ -4,5 +4,7 @@ import lib.validation.rules.Rule
 import lib.validation.rules.length
 
 class DigitsBetween(val digitRange: IntRange): Rule<Int> {
+    constructor(start: Int, end: Int): this(start..end)
+    
     override fun valid(value: Int): Boolean = digitRange.contains(value.length)
 }
