1106c2dd1e5705992b0284b92f4adc4cc80b4520
44x1carbon <b4079@oic.jp>
2018-03-28 14:52:04 +0900

    Exposed周りを整備

--- src/main/kotlin/lib/bootstrap/Database.kt
+++ src/main/kotlin/lib/bootstrap/Database.kt
@@ -1,14 +1,25 @@
 package lib.bootstrap
 
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.io.File
 import java.sql.Connection
 
 fun databaseSetup(): Unit {
-    Database.connect("jdbc:sqlite:${File("src/resources/database.sqlite").absolutePath}", "org.sqlite.JDBC")
-    transaction(transactionIsolation = Connection.TRANSACTION_SERIALIZABLE, repetitionAttempts = 3) {
+    Database.connect("jdbc:sqlite:${File("src/main/resources/database.sqlite").absolutePath}", "org.sqlite.JDBC")
+    transactionSQLite {
         logger.addLogger(StdOutSqlLogger)
+    }
+}
+
+fun Table.createTable() {
+    transactionSQLite {
+        SchemaUtils.create(this@createTable)
+    }
+}
+
+fun <T> transactionSQLite(block: Transaction.() -> T): T {
+    return transaction(transactionIsolation = Connection.TRANSACTION_SERIALIZABLE, repetitionAttempts = 3) {
+        block()
     }
 }
